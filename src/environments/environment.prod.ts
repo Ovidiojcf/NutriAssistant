172d4eeb6e9cda7b943c25db33a4d8d42c931ac0
Ovidiojcf <ojcfarias@gmail.com>
2024-06-11 15:09:33 -0300

    firabase banco

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+   firebaseConfig : {
+    apiKey: "AIzaSyDRaQegy6Kp2SB98HJsLmQRH9inXaEmQig",
+    authDomain: "nutriassistant-732c4.firebaseapp.com",
+    projectId: "nutriassistant-732c4",
+    storageBucket: "nutriassistant-732c4.appspot.com",
+    messagingSenderId: "587563881685",
+    appId: "1:587563881685:web:fbb4c448ac907c36823e5c"
+  }
 };
