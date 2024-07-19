bbf32c0f7f74d80a00a21d11448231248bb29aba
Ovidiojcf <ojcfarias@gmail.com>
2024-07-19 17:15:30 -0300

    refatorar ficha

---
+++ src/app/model/entities/formulario.ts
@@ -0,0 +1,22 @@
+export interface Formulario {
+
+    formularioId : string;
+    data: Date;
+    pIMC : string;
+    pPerda : string;
+    pReducao : string;
+    pEstado : string;
+    eNutricionalPrejudicado: string;
+    gravidadeDoenca: string;
+    pontos: number;
+    estadoNutricional: string; // refere-se a soma das pontuações das suas colunas nutricional prejudicado e gravidade da doença 
+    dietaPrescrita: string; // campo final da parte2 
+    mudancaPeso: string;
+    continuaPerdaPeso: string;
+    pesoAtual: string;
+    pesoHabitual: string;
+    tempo: string;
+    pesoInternacao: string;
+    diasInternado: string;
+
+}
